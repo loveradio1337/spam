28e25c979a182eafdd9d152146c2ea2536e15347
Love radio v616 <45826030+loveradio1337@users.noreply.github.com>
2018-12-18 08:28:41 +0000

    Create 6ix9ine.py

---
+++ 6ix9ine.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext.commands import bot
+from discord.ext import commands
+import random
+import asyncio
+import time
+print("Bot Is Online! And Ready To Spam")
+ 
+##SPAM COMMAND##
+	@bot.command(pass_context=True)
+	async def spam(ctx):
+	    while True:
+	        await bot.say("mv!ping")
+	        await bot.say("fuck")
+	        await bot.say("fuck")
+	        await bot.say("fuck")
+	        await bot.say("mv!ping")
+	        await bot.say("fuck")
+	        await bot.say("fuck")
+	        await bot.say("fuck")
+	        await bot.say("mv!ping")
+	        await bot.say("fuck")
+	        await bot.say("fuck")
+	        await bot.say("fuck")
+	        await bot.say("mv!ping")
+	
+	
+	bot.run(os.environ['Token'])
